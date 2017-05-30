849a16c370bf0fe08a7515b4850becbc2a11a061
caleb2004 <Caleb@HANNINGdeMacBook-Pro.local>
2017-05-30 03:52:26 -0300

    a2-stage1

--- app/src/main/java/com/example/caleb/test/MainActivity.java
+++ app/src/main/java/com/example/caleb/test/MainActivity.java
@@ -10,4 +10,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //check the first rule: if the password is "password"
+    public static boolean checkPW(String s){
+        return !s.equalsIgnoreCase("password");
+    }
+
+    //check the second rule: if the password length at least 8 characters
+    public static boolean checkLength(String s){
+        return s.length() > 7 ;
+    }
 }
+
--- app/src/test/java/com/example/caleb/test/ExampleUnitTest.java
+++ app/src/test/java/com/example/caleb/test/ExampleUnitTest.java
@@ -11,7 +11,12 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void validate1() throws Exception{
+        assertTrue(!MainActivity.checkPW("password");
+    }
+
+    @Test
+    public void validate2() throws Exception{
+        assertTrue(MainActivity.checkLength("password"));
     }
 }
