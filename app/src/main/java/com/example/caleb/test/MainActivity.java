c755b5e547886eff9722cb67df56de096a383f85
caleb2004 <Caleb@HANNINGdeMacBook-Pro.local>
2017-05-30 04:37:21 -0300

    a2-stage2

--- app/src/main/java/com/example/caleb/test/MainActivity.java
+++ app/src/main/java/com/example/caleb/test/MainActivity.java
@@ -10,4 +10,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public static boolean checkPassword(String s){
+        //check the first rule: if the password is "password"
+        if(s.equalsIgnoreCase("password")) return false;
+
+        //check the second rule: if the password length at least 8 characters
+        if(s.length() < 7) return false;
+
+        //check if the password contains a least 1 digit
+        if(!s.matches(".*[0-9]+.*")) return false;
+
+        //check if the password contains both upper and lower case
+        if(s.matches("[[!a-z]0-9_]+") || s.matches("[[!A-Z]0-9_]+")) return false;
+
+        //check if the password contains special characters
+        if(!s.matches(".*_.*") || s.matches(".*!.*") || s.matches(".*@.*")
+                || s.matches(".*@.*") || s.matches(".*#.*") || s.matches(".*%.*"))
+            return false;
+
+        return true;
+    }
+
+
 }
